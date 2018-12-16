41d1da6cfb4c7c84c91acb38a8ff84632969ddd1
alessandro rocha <brokersandro@gmail.com>
2018-12-16 11:43:34 -0300

    Primeiro commit

---
+++ Banco/src/br/com/bancorock/agencia.java
@@ -0,0 +1,20 @@
+package br.com.bancorock;
+
+public class agencia {
+	
+	int id_agencia;
+	String conta ;
+	String endereco;
+	int gerente ;
+	
+	
+	public agencia(int id_agencia, String conta, String endereco, int gerente) {
+		super();
+		this.id_agencia = id_agencia;
+		this.conta = conta;
+		this.endereco = endereco;
+		this.gerente = gerente;
+	}
+	
+	
+}
